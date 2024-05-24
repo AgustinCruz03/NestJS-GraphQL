03079043442f7acc442397d094ca09653e5533af
AgustinCruz03 <agustincruz0905@gmail.com>
2024-05-24 01:43:44 -0300

    entity users implementada

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,23 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  getUsers(): User[] {
-    return [
-      {
-        id: '59258632-0f94-481b-8468-043ad332368b',
-        username: 'juan',
-        email: 'juan@mail.com',
-        password: '123',
-        age: 40,
-      },
-      {
-        id: '0e3c7853-7053-4aaf-b732-8d120cf2d7ec',
-        username: 'pedro',
-        email: 'pedro@mail.com',
-        password: '123',
-      },
-    ];
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+  async getUsers(): Promise<User[]> {
+    return await this.userRepository.find();
   }
 }
